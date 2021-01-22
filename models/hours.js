512eb368e3447539cfd23aff48d2520c24770455
nlopez <nelly.lopez@utexas.edu>
2021-01-21 20:08:11 -0600

    updated hours model

--- models/hours.js
+++ models/hours.js
@@ -1,10 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Hours extends Model {}
+class Hours extends Model { }
 
 Hours.init(
     {
-        
+        //columns
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        total_hours: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        billable_hours: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id"'
+            }
+        },
+        unbillable_hours: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+
+    },
+    {
+       sequelize,
+       freezeTableName: true,
+       underscored: true,
+       modelName: 'hours' 
     }
-)
+);
+
+module.exports = Hours;
