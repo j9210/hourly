2aa9a586ca995f9770667fae2f4fc5465778e03d
shaggy <simmons5family@gmail.com>
2021-01-24 20:34:55 -0600

    signUp js logic & link in homepage.handlebars

---
+++ public/javascript/signUp.js
@@ -0,0 +1,27 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signUp').value.trim();
+    const email = document.querySelector('#email-signUp').value.trim();
+    const password = document.querySelector('#password-signUp').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      
+      if(response.ok) {
+          console.log('success');
+          alert("You've been signed up");
+      } else {
+          alert(response.statusText)
+      }
+    }
+};
+document.querySelector('#sign-up').addEventListener('submit', signupFormHandler);
