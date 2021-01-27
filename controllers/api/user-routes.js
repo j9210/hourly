8968660baea604208123912bfce6ffe0783ade14
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-26 20:22:05 -0500

    added login and logout routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -31,5 +31,68 @@ router.post('/', (req, res) => {
         });
     });
 
+    //login
+    router.post('/login', (req, res) => {
+        // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+        User.findOne({
+          where: {
+            email: req.body.email
+          }
+        }).then(dbUserData => {
+          if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+          }
+      
+          const validPassword = dbUserData.checkPassword(req.body.password);
+      
+          if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+      
+          req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+          });
+        });
+      });
+
+      //logout
+      router.post('/logout', (req, res) => {
+        if (req.session.loggedIn) {
+          req.session.destroy(() => {
+            res.status(204).end();
+          });
+        }
+        else {
+          res.status(404).end();
+        }
+      });
+      
+      router.put('/:id', (req, res) => {
+
+        User.update(req.body, {
+          individualHooks: true,
+          where: {
+            id: req.params.id
+          }
+        })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+
 
     module.exports = router;
