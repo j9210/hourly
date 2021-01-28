0a3294f73b5a6bc240babdb134fc5f3fccd84f22
Xavier <X64515@yahoo.com>
2021-01-28 13:56:20 -0600

    changes made to server with tutor may have Database issues on my side working to resolve

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-
+console.log(process.env.DB_NAME, process.env.DB_PW, process.env.DB_USER)
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3301
+    port: 3306
   });
 
 module.exports = sequelize;
