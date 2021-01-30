b0feda9293bf893bf25b2aa05758ce1a04019e1b
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-29 23:25:04 -0500

    can navigate between login, signup, dashboard, and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,31 +3,7 @@ const sequelize = require('../config/connection');
 const {  User, Hours , Project } = require('../models');
 
 router.get('/', (req, res) => {
-  console.log('======================');
-  Project.findAll({
-    where: { user_id : req.session.user_id}
-
-    ,
-    include: [
-      {
-        model: User,
-        attributes: ['id', 'username'],
-
-      },
-    ]
-  })
-    .then(dbprojects => {
-      const projects = dbprojects.map(projects => projects.get({ plain: true }));
-
-      res.render('homepage', {
-        projects,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
