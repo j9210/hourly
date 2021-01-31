cbda49504ef52c2d7ce99c8e4d348bb243bd764c
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-31 14:36:04 -0500

    need to fix function in hours js

--- public/javascript/hours.js
+++ public/javascript/hours.js
@@ -1,11 +1,18 @@
 // need to find difference bw these 2 vars to get hours using dayjs
-const startTime = dayjs(document.querySelector('input[name="start-time"]').value.trim()).format('hh mm A');
-const endTime = dayjs(document.querySelector('input[name="end-time"]').value.trim()).format('hh mm A');
+// const startTime = dayjs(document.querySelector('input[name="start-time"]').value.trim()).format('hh mm A');
+// const endTime = dayjs(document.querySelector('input[name="end-time"]').value.trim()).format('hh mm A');
+const hoursBtn = document.querySelector('#add-hours');
 
 
 
 async function addHours(event) {
   event.preventDefault();
+
+const startTime = document.querySelector('#start-time');
+const endTime = document.querySelector('#end-time');
+ 
+console.log(startTime);
+console.log(endTime);
 
   const response = await fetch('/api/hours', {
     method: 'post',
@@ -23,5 +30,7 @@ async function addHours(event) {
         alert(response.statusText)
     }
 }
+
+hoursBtn.addEventListener('click', addHours);
 
 // async function deleteHours(event) {}
