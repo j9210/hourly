0a402665f4b69a5fa7cb120932a1f743c4cf22d1
j9210 <71297542+j9210@users.noreply.github.com>
2021-01-31 11:50:39 -0600

    hours functionality w notes about what needs to be done

--- public/javascript/hours.js
+++ public/javascript/hours.js
@@ -1,6 +1,27 @@
-const hours = {
-  id: 1,
-  user_id: 1,
-  billable_hours: 1,
-  unbillable_hours: 1
+// need to find difference bw these 2 vars to get hours using dayjs
+const startTime = dayjs(document.querySelector('input[name="start-time"]').value.trim()).format('hh mm A');
+const endTime = dayjs(document.querySelector('input[name="end-time"]').value.trim()).format('hh mm A');
+
+
+
+async function addHours(event) {
+  event.preventDefault();
+
+  const response = await fetch('/api/hours', {
+    method: 'post',
+    body: JSON.stringify({
+      billable_hours,
+      unbillable_hours
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+    if(response.ok) {
+      console.log('Hours added!')
+      alert('Your hours have been added!')
+    } else {
+        alert(response.statusText)
+    }
 }
+
+// async function deleteHours(event) {}
