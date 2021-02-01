79d94794b426133f5cf2665575eddefa2aea434f
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-02-01 10:11:44 -0500

    added jaws need to connection js/need to add it in heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,29 @@
 // import the Sequelize constructor from the library
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+//console.log(process.env.DB_NAME, process.env.DB_PW, process.env.DB_USER)
+// create connection to our db
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+
+//module.exports = sequelize;
+
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
-//console.log(process.env.DB_NAME, process.env.DB_PW, process.env.DB_USER)
+
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
